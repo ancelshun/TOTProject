bae6469944e97316bd8e0b45a0ca5cedd52970c2
Ancel <ancelshun@qq.com>
2017-03-16 11:19:16 +0800

    create project

---
+++ TOTBase/TOTBase/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TOTBase
+//
+//  Created by Ancel on 16/03/2017.
+//  Copyright © 2017 pingan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
